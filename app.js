7033b38fbf153e2affef6158e6506330355db9fd
Jay Zuidema <zuidejay@gmail.com>
2019-03-05 19:28:40 +0000

    added landing and search routes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/search", function (req, res) {
+   res.render("search"); 
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server has started!!!");
 });
