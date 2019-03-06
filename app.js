d3e6f02b92ac82245df5d2d36222359013f1f346
Jay Zuidema <zuidejay@gmail.com>
2019-03-06 20:29:44 +0000

    added mongoose, movie model, and some routes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var express = require("express"),
-    app = express();
-    
+const express = require("express"),
+      app = express(),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      Movie = require("./models/movie");
+      
+require('dotenv').config();
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});    
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
@@ -10,6 +16,42 @@ app.get("/", function(req, res) {
 
 app.get("/search", function (req, res) {
    res.render("search"); 
+});
+
+app.get("/movies", function(req, res) {
+   Movie.find({}, function(err, foundMovies) {
+       if(err) {
+           console.log(err);
+       } else {
+           res.render("movies", {movies: foundMovies});
+       }
+   });
+});
+
+app.post("/movies", function(req, res) {
+    var title = req.body.title;
+    var director = req.body.director;
+    var poster = req.body.poster;
+    var year = req.body.year;
+    
+    var newMovie = {
+        title: title,
+        director: director,
+        poster: poster,
+        year: year
+    };
+    
+    Movie.create(newMovie, function(err, movie) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.redirect("/movies");
+       }
+    });
+});
+
+app.get("/movies/new", function(req, res){
+   res.render("new"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
