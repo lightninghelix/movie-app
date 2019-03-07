550270afc055e5b821659a10376ecf1c8f0a1ae1
Jay Zuidema <zuidejay@gmail.com>
2019-03-07 21:12:05 +0000

    added review model and review routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express"),
       app = express(),
       bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
-      Movie = require("./models/movie");
+      Movie = require("./models/movie"),
+      Review = require("./models/review");
       
 require('dotenv').config();
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});    
@@ -23,7 +24,7 @@ app.get("/movies", function(req, res) {
        if(err) {
            console.log(err);
        } else {
-           res.render("movies", {movies: foundMovies});
+           res.render("movies/index", {movies: foundMovies});
        }
    });
 });
@@ -33,12 +34,14 @@ app.post("/movies", function(req, res) {
     var director = req.body.director;
     var poster = req.body.poster;
     var year = req.body.year;
+    var synopsis = req.body.synopsis
     
     var newMovie = {
         title: title,
         director: director,
         poster: poster,
-        year: year
+        year: year,
+        synopsis: synopsis
     };
     
     Movie.create(newMovie, function(err, movie) {
@@ -51,7 +54,46 @@ app.post("/movies", function(req, res) {
 });
 
 app.get("/movies/new", function(req, res){
-   res.render("new"); 
+   res.render("movies/new"); 
+});
+
+app.get("/movies/:id", function(req, res) {
+    Movie.findById(req.params.id).populate("reviews").exec(function(err, foundMovie) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.render("movies/show", {movie: foundMovie});
+       }
+    });
+});
+
+app.get("/movies/:id/reviews/new", function(req, res) {
+    Movie.findById(req.params.id, function(err, movie) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("reviews/new", {movie: movie});
+        }
+    });
+});
+
+app.post("/movies/:id/reviews", function(req, res) {
+   Movie.findById(req.params.id, function(err, movie) {
+       if (err) {
+           console.log(err);
+           res.redirect("/movies");
+       } else {
+           Review.create(req.body.review, function(err, review) {
+              if (err) {
+                  console.log(err);
+              } else {
+                  movie.reviews.push(review);
+                  movie.save();
+                  res.redirect("/movies/" + movie._id);
+              }
+           });
+       }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
--- models/movie.js
+++ models/movie.js
@@ -4,7 +4,14 @@ var movieSchema = new mongoose.Schema({
     title: String,
     director: String,
     release_year: Number,
-    poster: String
+    poster: String,
+    synopsis: String,
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
