d30b1e38563e4b502d916ff1bb3aaba993ec11ec
Jay Zuidema <zuidejay@gmail.com>
2019-03-07 23:00:06 +0000

    added authentication

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require("express"),
       app = express(),
       bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
       Movie = require("./models/movie"),
-      Review = require("./models/review");
+      Review = require("./models/review"),
+      User = require("./models/user");
       
 require('dotenv').config();
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+app.use(require("express-session")({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -67,7 +81,7 @@ app.get("/movies/:id", function(req, res) {
     });
 });
 
-app.get("/movies/:id/reviews/new", function(req, res) {
+app.get("/movies/:id/reviews/new", isLoggedIn, function(req, res) {
     Movie.findById(req.params.id, function(err, movie) {
         if (err) {
             console.log(err);
@@ -77,7 +91,7 @@ app.get("/movies/:id/reviews/new", function(req, res) {
     });
 });
 
-app.post("/movies/:id/reviews", function(req, res) {
+app.post("/movies/:id/reviews", isLoggedIn, function(req, res) {
    Movie.findById(req.params.id, function(err, movie) {
        if (err) {
            console.log(err);
@@ -95,6 +109,46 @@ app.post("/movies/:id/reviews", function(req, res) {
        }
    });
 });
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/movies"); 
+        });
+    });
+});
+
+app.get("/login", function(req, res) {
+   res.render("login"); 
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/movies",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/movies"); 
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server has started!!!");
